3b02c8b627f58feb118f552debdf621fe221c312
Your Name <bonguyen0124@gmail.com>
2020-03-31 01:32:39 +0700

    first commit

---
+++ Header_page.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Header_page.ascx.cs" Inherits="Lich_su_viet_nam.Header_page" %>
---
+++ Header_page.ascx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lich_su_viet_nam
+{
+    public partial class Header_page : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
